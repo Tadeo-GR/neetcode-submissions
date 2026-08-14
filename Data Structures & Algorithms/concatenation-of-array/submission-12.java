4afbefe753d49a9f8be4d49251022a29c686ddd1
Tadeo-GR <gomezrodrigueztadeo@gmail.com>
2026-08-14 11:34:00 -0600

    Add: concatenation-of-array - submission-12

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-12.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        // DEFENSIVE CODE
+     if (nums==null){
+        return null;
+        }
+    if (nums.length==0){
+        return new int[0];
+    }
+    int n= nums.length ;
+    int[]ans = new int[2*n];
+    for (int i=0 ; i<n ; i++){
+        ans[i]=nums[i];
+        ans[i+n]=nums[i];
+
+       }
+       return ans;
+    }   
+
+}
