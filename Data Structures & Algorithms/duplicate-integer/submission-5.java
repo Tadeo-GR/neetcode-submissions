06d67206050bab6a7fffbe860ed5640b14f4ad8c
Tadeo-GR <gomezrodrigueztadeo@gmail.com>
2026-08-15 16:26:17 -0600

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums==null || nums.length==0){
+            return false;
+        }
+        Arrays.sort(nums);
+    for(int i=1 ; i< nums.length ; i++)
+    if(nums[i-1] == nums[i]){
+        return true;
+    }
+ return false;
+    }
+}
