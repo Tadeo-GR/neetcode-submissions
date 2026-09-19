f56100287dd9814d976dd0ac0a9992605efb4b22
Tadeo-GR <gomezrodrigueztadeo@gmail.com>
2026-09-19 16:43:38 -0600

    Add: concatenation-of-array - submission-31

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-31.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+    int n= nums.size();
+    vector<int>duplicate (2*n);
+    for(int i=0 ; i<n ; i++){
+        duplicate[i] = nums[i];
+        duplicate[i+n] = nums[i];
+    }
+    return duplicate;
+    }
+};
